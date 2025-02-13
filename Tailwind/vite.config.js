f358a7de0f30e349419660aaa754105bb78a90f5
Syed Tasavour <syedtasavour86@gmail.com>
2025-02-14 00:57:42 +0530

    Add Tailwind CSS configuration and integrate with Vite and React setup

--- Tailwind/vite.config.js
+++ Tailwind/vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(),tailwindcss(),],
+  
 })
