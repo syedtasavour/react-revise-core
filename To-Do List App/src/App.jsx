7293dfb9309dcbe8b7713367af8f69e1c31e25c9
Syed Tasavour <syedtasavour86@gmail.com>
2025-02-24 10:21:32 +0530

    Implement Todo management features with localStorage integration and create TodoForm and TodoItem components

--- To-Do List App/src/App.jsx
+++ To-Do List App/src/App.jsx
@@ -1,19 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./context/TodoContext";
+import {TodoForm, TodoItem} from "./components/index";
 
 function App() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todo"));
+    if (todos && todos.length) {
+      setTodos(todos);
+    }
+  },[]);
+useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [
+      {
+        id: Date.now(),
+        ...todo,
+      },
+      ...prev,
+    ]);
+  };
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prev.id === id ? todo : prevTodo))
+    );
+  };
+  const deletetodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : prev
+      )
+    );
+  };
+
   return (
-    <TodoProvider value={{todos,addTodos,deletetodo}}>
+    <TodoProvider
+      value={{ todos, addTodo, deletetodo, toggleComplete, updateTodo }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4"><TodoForm/></div>
           <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
+            {todos.map((todo)=>(
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo}/>
+               </div>
+            ))}
           </div>
         </div>
       </div>
--- To-Do List App/src/context/TodoContext.jsx
+++ To-Do List App/src/context/TodoContext.jsx
@@ -4,7 +4,7 @@ export const TodoContext = createContext({
   todos: [{ id: 1, todo: "msg here", completed: false }],
   addTodo: (todo) => {},
   updateTodo: (id, todo) => {},
-  deletetodo: (id) => {},
+  deleteTodo: (id) => {},
   toggleComplete: (id) => {},
 });
 
